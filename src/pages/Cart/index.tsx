e46ad6bca8579cdb14b48a62ea4d943d9de4e019
Wevison <wevisonramalho@gmail.com>
2022-04-05 18:01:10 -0300

    [ FEATURE ] Styled Cart.

--- src/pages/Cart/index.tsx
+++ src/pages/Cart/index.tsx
@@ -1,10 +1,73 @@
 import React from 'react';
+import {
+  MdRemoveCircleOutline,
+  MdAddCircleOutline,
+  MdDelete,
+} from 'react-icons/md';
 
-import ContainerCart from './styles';
+import { ContainerCart, ProductTable, Product, Footer, Total } from './styles';
 
 export const Cart = () => (
   <ContainerCart>
-    <p>Sou o Carrinho!</p>
+    <ProductTable>
+      <thead className="head">
+        <tr className="row">
+          <th className="column" />
+          <th className="column">Produto</th>
+          <th className="column">Qtd</th>
+          <th className="column">Subtotal</th>
+          <th className="column" />
+        </tr>
+      </thead>
+
+      <tbody className="body">
+        <Product className="row">
+          <td className="column">
+            <img
+              src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSNFZ2E2HCN4wf9I07oS2f-qVH9ZZaHHY0CWenwMOMZzcfcgFVrhTCwhEx0frYkwDvUWLQuRwOMPw&usqp=CAc"
+              alt="Tênis"
+              className="image"
+            />
+          </td>
+          <td className="column">
+            <strong className="title">Tênis</strong>
+            <span className="text">R$129,90</span>
+          </td>
+          <td className="column">
+            <div className="wrapper-qtd">
+              <button type="button" className="button">
+                <MdRemoveCircleOutline size={20} color="#7159C1" />
+              </button>
+
+              <input type="number" readOnly value={1} className="input" />
+
+              <button type="button" className="button">
+                <MdAddCircleOutline size={20} color="#7159C1" />
+              </button>
+            </div>
+          </td>
+          <td className="column">
+            <strong className="subtotal">R$258,80</strong>
+          </td>
+          <td className="column">
+            <button type="button" className="button">
+              <MdDelete size={20} color="#7159C1" />
+            </button>
+          </td>
+        </Product>
+      </tbody>
+    </ProductTable>
+
+    <Footer>
+      <button type="button" className="button">
+        Finalizar pedido
+      </button>
+
+      <Total>
+        <span className="title">TOTAL</span>
+        <strong className="text">R$1920,28</strong>
+      </Total>
+    </Footer>
   </ContainerCart>
 );
 
--- src/pages/Cart/styles.js
+++ src/pages/Cart/styles.js
@@ -1,5 +1,111 @@
 import styled from 'styled-components';
 
-const ContainerCart = styled.div``;
+import { darken } from 'polished';
 
-export default ContainerCart;
+export const ContainerCart = styled.div`
+  padding: 30px;
+  background-color: #ffffff;
+  border-radius: 4px;
+`;
+
+export const ProductTable = styled.table`
+  width: 100%;
+
+  .head {
+    > .row {
+      color: #999;
+      text-align: left;
+      padding-top: 12px;
+      text-transform: uppercase;
+    }
+  }
+
+  .body {
+    > .row {
+      .column {
+        padding-top: 12px;
+        border-bottom: 1px solid #eeeeee;
+      }
+    }
+  }
+`;
+
+export const Product = styled.tr`
+  .image {
+    height: 100px;
+  }
+
+  .title {
+    color: #777777;
+    display: block;
+  }
+
+  .text {
+    display: block;
+    margin-top: 5px;
+    font-size: 18px;
+    font-weight: bold;
+    color: #151515;
+  }
+
+  .subtotal {
+    color: #151515;
+  }
+
+  .wrapper-qtd {
+    display: flex;
+    align-items: center;
+
+    .input {
+      border: 1px solid #dddddd;
+      border-radius: 4px;
+      color: #666666;
+      padding: 6px;
+      width: 50px;
+    }
+  }
+
+  .button {
+    background-color: transparent;
+    border: 0;
+    padding: 6px;
+  }
+`;
+
+export const Footer = styled.footer`
+  margin-top: 30px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  .button {
+    background-color: #7159c1;
+    color: #ffffff;
+    border: 0;
+    border-radius: 4px;
+    padding: 12px 20px;
+    font-weight: bold;
+    text-transform: uppercase;
+    transition: 0.35s background-color;
+
+    &:hover {
+      background-color: ${darken(0.05, '#7159C1')};
+    }
+  }
+`;
+
+export const Total = styled.footer`
+  display: flex;
+  align-items: baseline;
+
+  .title {
+    color: #999999;
+    font-weight: bold;
+  }
+
+  .text {
+    font-size: 28px;
+    margin-left: 5px;
+    color: #151515;
+  }
+`;
