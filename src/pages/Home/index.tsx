7974a986101f8773e3e3a9395138d5bd2770d516
Wevison <wevisonramalho@gmail.com>
2022-04-06 12:30:16 -0300

    [ FEATURE ] Consuming API.

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,113 +1,62 @@
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import { MdAddShoppingCart } from 'react-icons/md';
 
 import { ProductList, Product } from './styles';
 
+import api from '../../services/api';
+
+import { formatPrice } from '../../utils/format';
+interface Product {
+  id: number;
+  image: string;
+  title: string;
+  price: number;
+  priceFormated: string;
+}
+
 const Home = () => {
+  const [dataApi, setDataApi] = useState([]);
+
+  const getProducts = async () => {
+    let { data } = await api.get('/products');
+
+    data = data.map((product: Product) => {
+      return { ...product, priceFormated: formatPrice(product.price) };
+    });
+
+    setDataApi(data);
+  };
+
+  useEffect(() => {
+    getProducts();
+  }, []);
+
+  console.log(dataApi);
+
   return (
     <ProductList>
-      <Product>
-        <div className="container-image">
-          <img
-            src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSNFZ2E2HCN4wf9I07oS2f-qVH9ZZaHHY0CWenwMOMZzcfcgFVrhTCwhEx0frYkwDvUWLQuRwOMPw&usqp=CAc"
-            alt="Tênis"
-            className="image"
-          />
-        </div>
-        <strong className="title">Tênis</strong>
-        <span className="text">R$129,90</span>
+      {dataApi &&
+        dataApi.map((product: Product) => {
+          return (
+            <Product key={product.id}>
+              <div className="container-image">
+                <img src={product.image} alt="Tênis" className="image" />
+              </div>
+              <strong className="title">{product.title}</strong>
+              <span className="text">{product.priceFormated}</span>
 
-        <button type="button" className="button">
-          <div className="wrapper">
-            <MdAddShoppingCart size={16} color="#FFFFFF" /> 3
-          </div>
+              <button type="button" className="button">
+                <div className="wrapper">
+                  <MdAddShoppingCart size={16} color="#FFFFFF" /> 3
+                </div>
 
-          <span className="text">Adicionar ao carrinho</span>
-        </button>
-      </Product>
-
-      <Product>
-        <div className="container-image">
-          <img
-            src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSNFZ2E2HCN4wf9I07oS2f-qVH9ZZaHHY0CWenwMOMZzcfcgFVrhTCwhEx0frYkwDvUWLQuRwOMPw&usqp=CAc"
-            alt="Tênis"
-            className="image"
-          />
-        </div>
-        <strong className="title">Tênis</strong>
-        <span className="text">R$129,90</span>
-
-        <button type="button" className="button">
-          <div className="wrapper">
-            <MdAddShoppingCart size={16} color="#FFFFFF" /> 3
-          </div>
-
-          <span className="text">Adicionar ao carrinho</span>
-        </button>
-      </Product>
-
-      <Product>
-        <div className="container-image">
-          <img
-            src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSNFZ2E2HCN4wf9I07oS2f-qVH9ZZaHHY0CWenwMOMZzcfcgFVrhTCwhEx0frYkwDvUWLQuRwOMPw&usqp=CAc"
-            alt="Tênis"
-            className="image"
-          />
-        </div>
-        <strong className="title">Tênis</strong>
-        <span className="text">R$129,90</span>
-
-        <button type="button" className="button">
-          <div className="wrapper">
-            <MdAddShoppingCart size={16} color="#FFFFFF" /> 3
-          </div>
-
-          <span className="text">Adicionar ao carrinho</span>
-        </button>
-      </Product>
-
-      <Product>
-        <div className="container-image">
-          <img
-            src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSNFZ2E2HCN4wf9I07oS2f-qVH9ZZaHHY0CWenwMOMZzcfcgFVrhTCwhEx0frYkwDvUWLQuRwOMPw&usqp=CAc"
-            alt="Tênis"
-            className="image"
-          />
-        </div>
-        <strong className="title">Tênis</strong>
-        <span className="text">R$129,90</span>
-
-        <button type="button" className="button">
-          <div className="wrapper">
-            <MdAddShoppingCart size={16} color="#FFFFFF" /> 3
-          </div>
-
-          <span className="text">Adicionar ao carrinho</span>
-        </button>
-      </Product>
-
-      <Product>
-        <div className="container-image">
-          <img
-            src="https://encrypted-tbn1.gstatic.com/shopping?q=tbn:ANd9GcSNFZ2E2HCN4wf9I07oS2f-qVH9ZZaHHY0CWenwMOMZzcfcgFVrhTCwhEx0frYkwDvUWLQuRwOMPw&usqp=CAc"
-            alt="Tênis"
-            className="image"
-          />
-        </div>
-        <strong className="title">Tênis</strong>
-        <span className="text">R$129,90</span>
-
-        <button type="button" className="button">
-          <div className="wrapper">
-            <MdAddShoppingCart size={18} color="#FFFFFF" />{' '}
-            <span className="itens">3</span>
-          </div>
-
-          <span className="text">Adicionar ao carrinho</span>
-        </button>
-      </Product>
+                <span className="text">Adicionar ao carrinho</span>
+              </button>
+            </Product>
+          );
+        })}
     </ProductList>
   );
 };
